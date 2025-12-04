99cffb6d0831e11642ac214ee141b2c2ffdd7ded
AziAzi22 <aziazi22t@gmail.com>
2025-12-04 14:39:49 +0500

    end

--- router/fastfood.routes.js
+++ router/fastfood.routes.js
@@ -7,6 +7,6 @@ fastfoodRouter.get("/get_menu", getMenu)
 fastfoodRouter.get("/get_one_food/:id", getOneFood)
 fastfoodRouter.post("/add_new_food", addNewFood)
 fastfoodRouter.post("/update_food/:id", updateFood)
-fastfoodRouter.post("delete_food/:id", deleteFood)
+fastfoodRouter.post("/delete_food/:id", deleteFood)
 
 module.exports = fastfoodRouter
