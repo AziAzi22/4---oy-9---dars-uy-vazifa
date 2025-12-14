760c051c9c083beaf799b53e78c49e5f91983d7c
AziAzi22 <aziazi22t@gmail.com>
2025-12-14 19:26:14 +0500

    chpt git push

--- controller/fastfood.controller.js
+++ controller/fastfood.controller.js
@@ -1,44 +1,41 @@
 const { v4 } = require("uuid");
 const { read_file, write_file } = require("../fs/file-manager");
-const menu = read_file("fastfood.json");
+
 const myFind = (arr, id) => arr.find((food) => food.id === id);
 
 /// get menu
-
 const getMenu = async (req, res) => {
   try {
-    res.status(200).render("index", { menu });
+    const menu = read_file("fastfood.json");
+    res.render("index", { menu });
   } catch (error) {
-    res.status(500).json({
-      message: error.message,
-    });
+    res.status(500).json({ message: error.message });
   }
 };
 
 /// get one food
-
 const getOneFood = async (req, res) => {
   try {
+    const menu = read_file("fastfood.json");
     const { id } = req.params;
     const foundedFood = myFind(menu, id);
 
     if (!foundedFood) {
-      return res.status(302).redirect("/get_menu");
+      return res.redirect("/get_menu");
     }
 
-    res.status(200).render("details", { foundedFood });
+    res.render("details", { foundedFood });
   } catch (error) {
-    res.status(500).json({
-      message: error.message,
-    });
+    res.status(500).json({ message: error.message });
   }
 };
 
 /// add new food
-
 const addNewFood = async (req, res) => {
   try {
+    const menu = read_file("fastfood.json");
     const { title, price, discription, calories } = req.body;
+
     menu.push({
       id: v4(),
       title,
@@ -49,67 +46,57 @@ const addNewFood = async (req, res) => {
     });
 
     write_file("fastfood.json", menu);
-    res.status(312).redirect("/get_menu");
+    res.redirect("/get_menu");
   } catch (error) {
-    res.status(500).json({
-      message: error.message,
-    });
+    res.status(500).json({ message: error.message });
   }
 };
 
 /// update food
-
 const updateFood = async (req, res) => {
   try {
+    const menu = read_file("fastfood.json");
     const { id } = req.params;
     const foundedFood = myFind(menu, id);
     const { title, price, discription, calories } = req.body;
 
     if (!foundedFood) {
-      return res.status(302).redirect("/get_menu");
+      return res.redirect("/get_menu");
     }
 
     menu.forEach((food) => {
       if (food.id === id) {
-        food.title = title ? title : food.title;
-        food.price = price ? price : food.price;
-        food.discription = discription ? discription : food.discription;
-        food.calories = calories ? calories : food.calories;
+        food.title = title ?? food.title;
+        food.price = price ?? food.price;
+        food.discription = discription ?? food.discription;
+        food.calories = calories ?? food.calories;
       }
     });
 
     write_file("fastfood.json", menu);
-    res.status(302).redirect("/get_menu");
+    res.redirect("/get_menu");
   } catch (error) {
-    res.status(500).json({
-      message: error.message,
-    });
+    res.status(500).json({ message: error.message });
   }
 };
 
-/// delete food redirect
-
+/// delete food
 const deleteFood = async (req, res) => {
   try {
+    const menu = read_file("fastfood.json");
     const { id } = req.params;
     const foundedFood = myFind(menu, id);
 
     if (!foundedFood) {
-      res.status(302).redirect("/get_menu");
+      return res.redirect("/get_menu");
     }
 
-    menu.forEach((food, i) => {
-      if (food.id === id) {
-        menu.splice(i, 1);
-      }
-    });
+    const newMenu = menu.filter((food) => food.id !== id);
 
-    write_file("fastfood.json", menu);
-    res.status(302).redirect("/get_menu");
+    write_file("fastfood.json", newMenu);
+    res.redirect("/get_menu");
   } catch (error) {
-    res.status(500).json({
-      message: error.message,
-    });
+    res.status(500).json({ message: error.message });
   }
 };
 
